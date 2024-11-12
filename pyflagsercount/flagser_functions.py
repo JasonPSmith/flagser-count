d4f7782a2c6a424ed693ebd31f4833c7122f8748
jlazovskis <jlazovskis@gmail.com>
2024-11-12 16:54:11 +0000

    Revert "undirected option for python". Unsure of implications, undoing
    This reverts commit b461c2b61746b39b99040f5c6efc16a295e85613.

--- pyflagsercount/flagser_functions.py
+++ pyflagsercount/flagser_functions.py
@@ -3,12 +3,12 @@ import numpy as np
 from pycount import run_flagser_count
 
 def flagser_count(adjacency_matrix,max_simplices=False,containment=False,
-                  return_simplices=False, unidrected=False, print='',threads=8,transpose=False,out='',binary='',max_dim=-1,
+                  return_simplices=False, print='',threads=8,transpose=False,out='',binary='',max_dim=-1,
                   vertices_todo='',max_dim_print=-1,min_dim_print=-1, compressed=False, est_max_dim=-1,
                   edge_containment=False):
 
     return run_flagser_count(adjacency_matrix.shape[0], np.transpose(np.array(np.nonzero(adjacency_matrix))),
-                             max_simplices,containment,return_simplices, undirected, (len(print)>0),print,
+                             max_simplices,containment,return_simplices,(len(print)>0),print,
                              str(threads),transpose,(len(out)>0),out,(len(binary)>0),binary,
                              (max_dim!=-1),str(max_dim),(len(vertices_todo)>0),vertices_todo,
                              (max_dim_print!=-1),str(max_dim_print),(min_dim_print!=-1),str(min_dim_print),
@@ -16,12 +16,12 @@ def flagser_count(adjacency_matrix,max_simplices=False,containment=False,
                              edge_containment)
 
 def flagser_count_edges(num_vertices, edge_list, max_simplices=False,containment=False,
-                  return_simplices=False, undirected=False, print='',threads=8,transpose=False,out='',binary='',max_dim=-1,
+                  return_simplices=False, print='',threads=8,transpose=False,out='',binary='',max_dim=-1,
                   vertices_todo='',max_dim_print=-1,min_dim_print=-1, compressed=False, est_max_dim=-1,
                   edge_containment=False):
 
     return run_flagser_count(num_vertices, edge_list,
-                             max_simplices,containment,return_simplices, undirected, (len(print)>0),print,
+                             max_simplices,containment,return_simplices,(len(print)>0),print,
                              str(threads),transpose,(len(out)>0),out,(len(binary)>0),binary,
                              (max_dim!=-1),str(max_dim),(len(vertices_todo)>0),vertices_todo,
                              (max_dim_print!=-1),str(max_dim_print),(min_dim_print!=-1),str(min_dim_print),
